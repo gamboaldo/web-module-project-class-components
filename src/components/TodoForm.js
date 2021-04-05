e02d9af61e40603f0adbf0500f6a7e8b6335901a
ALDO <aldo@aldos-mbp.attlocal.net>
2021-04-05 13:47:44 -0700

    form has input

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
 
 export default class TodoForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+    };
+  }
+
   render() {
     return (
       <div>
         <form onSubmit={""}>
+          <input type="text" name="item" />
           <button>Add</button>
         </form>
       </div>
